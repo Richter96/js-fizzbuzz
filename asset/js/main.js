c16a0c22e355a75a2535192a2d2bbe5885b39f06
Richter96 <castiglione96.rc@gmail.com>
2023-03-10 16:47:53 +0100

    add if element

--- asset/js/main.js
+++ asset/js/main.js
@@ -10,24 +10,35 @@ Abbiamo usato un operatore  nei giorni scorsi che possiamo riusare in questi ese
 
 */
 
-// creo ul in dom
-const ulElement = document.createElement("ul")
-console.log(ulElement)
 
+/* Scrivi un programma che stampi in console i numeri da 1 a 100 ma che:
+- per i multipli di 3 stampi “Fizz” al posto del numero
+- per i multipli di 5 stampi “Buzz”
+- per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
 
-const BodyElement = document.querySelector("body")
-BodyElement.append(ulElement)
+//Scrivere in console.log numeri da 1 a 100
+const ulElement = document.createElement("ul");
+const liElement = document.createElement("li")
 
-for (let i = 0; i < 50; i++) {
-    // console.log(i)
+for (let i = 1; i < 101; i++) {
 
-    const liElement = document.createElement("li")
-// all'liElement do valore i
-    liElement.append(i)
-// all'liElement do valore i
-    ulElement.append(liElement)
-    
-    if ( i % 3 == 0) {
-        i = ('Frizz')
-    } else 
+    if (i % 3 == 0) {
+        
+    } else if (i % 5 == 0) {
+        
+    } else if (i % 3 == 0 && i % 5 == 0) {
+        
+    }
+    else if (i % 3 == 0 && i % 5 == 0) {
+        
+    }
+    liElement.append(`${i}${fizz}${buzz}${fizzbuzz}`);
 }
+
+
+ulElement.append(liElement)
+
+console.log(ulElement);
+
+
+
